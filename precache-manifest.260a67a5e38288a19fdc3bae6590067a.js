686d5922eef13c34cc62ece0a12957e3dcbb3a8c
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-16 12:20:10 -0700

    Updates

---
+++ precache-manifest.260a67a5e38288a19fdc3bae6590067a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19a271906fd4fde926974e9bcfb76cf",
+    "url": "/trelloyes-mattP-seyi-/index.html"
+  },
+  {
+    "revision": "1fec71b0bddf08277eba",
+    "url": "/trelloyes-mattP-seyi-/static/css/main.89af4e8e.chunk.css"
+  },
+  {
+    "revision": "411747e8ca5d1f65fc92",
+    "url": "/trelloyes-mattP-seyi-/static/js/2.0f8b9328.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trelloyes-mattP-seyi-/static/js/2.0f8b9328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fec71b0bddf08277eba",
+    "url": "/trelloyes-mattP-seyi-/static/js/main.e8ac68de.chunk.js"
+  },
+  {
+    "revision": "54a8181cb57f5fb787f5",
+    "url": "/trelloyes-mattP-seyi-/static/js/runtime-main.a0f278e0.js"
+  }
+]);
